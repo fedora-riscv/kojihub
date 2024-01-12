28377170ed0e7c94e8cb6e4c0a401ca619b75c19
Guoguo <16666742+imguoguo@users.noreply.github.com>
2024-01-12 20:38:46 +0800

    Koji Builder branch init.

--- kojiweb/bin/setup_koji.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/opt/koji_certs_issue_scripts/koji_certs-issue.sh -C /opt/koji_certs_issue_scripts/koji_certs-issue_openkoji.conf -d
-/opt/koji_certs_issue_scripts/koji_certs-issue.sh -C /opt/koji_certs_issue_scripts/koji_certs-issue_openkoji.conf -c
-/opt/koji_certs_issue_scripts/koji_certs-issue.sh -C /opt/koji_certs_issue_scripts/koji_certs-issue_openkoji.conf -m
-
